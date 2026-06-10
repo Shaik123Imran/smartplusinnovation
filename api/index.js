b276f625f8cd5515d8f0d97a1bf30b4fad4abaac
Shaik123Imran <shaik123imran@example.com>
2026-06-10 11:04:40 +0530

    Debug: wrap Express in promise to catch async errors

--- api/index.js
+++ api/index.js
@@ -1,3 +1,18 @@
+function runApp(app, req, res) {
+  return new Promise((resolve, reject) => {
+    const _end = res.end.bind(res)
+    res.end = function (...args) {
+      resolve()
+      return _end(...args)
+    }
+    try {
+      app(req, res)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
 async function getApp() {
   const mod = await import('../server/app.js')
   return mod.default
@@ -7,7 +22,7 @@ export default async function handler(req, res) {
   console.log(`[API] ${req.method} ${req.url}`)
   try {
     const app = await getApp()
-    app(req, res)
+    await runApp(app, req, res)
   } catch (err) {
     console.error('[API] Error:', err.message, err.stack)
     res.statusCode = 500
