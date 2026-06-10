43e7bbb3c0448428f49c5a35f92c62dd9853a743
Shaik123Imran <shaik123imran@example.com>
2026-06-10 11:28:47 +0530

    Test: all 8 route imports

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from 'express'
+
+import authRoutes from '../server/routes/authRoutes.js'
+import userRoutes from '../server/routes/userRoutes.js'
 import courseRoutes from '../server/routes/courseRoutes.js'
+import testimonialRoutes from '../server/routes/testimonialRoutes.js'
+import formRoutes from '../server/routes/formRoutes.js'
+import adminRoutes from '../server/routes/adminRoutes.js'
+import paymentRoutes from '../server/routes/paymentRoutes.js'
+import videoRoutes from '../server/routes/videoRoutes.js'
 
 const app = express()
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
 app.use('/api/courses', courseRoutes)
+app.use('/api/testimonials', testimonialRoutes)
+app.use('/api/forms', formRoutes)
+app.use('/api/admin', adminRoutes)
+app.use('/api/payments', paymentRoutes)
+app.use('/api/videos', videoRoutes)
 app.get('/api/ping', (req, res) => res.json({ ping: 'ok' }))
 
 export default function handler(req, res) {
