a660d05bd68546c55bcfa64525c824294dbf9a5f
Shaik123Imran <shaik123imran@example.com>
2026-06-10 11:24:45 +0530

    Test: only import connectDB from server/

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import connectDB from '../server/config/db.js'
 
 const app = express()
-app.get('/api/ping', (req, res) => res.json({ ping: 'ok' }))
+app.get('/api/ping', (req, res) => res.json({ ping: 'ok', db: !!connectDB }))
 
 export default function handler(req, res) {
   console.log(`[API] ${req.method} ${req.url}`)
